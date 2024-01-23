eac0199432f8a28a59dc1b95d6d7892408068562
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2024-01-23 10:04:52 -0500

    install react-native-svg

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ npx expo install firebase
 npm install @react-navigation/native @react-navigation/native-stack
 npx expo install react-native-screens react-native-safe-area-context
 
+#SVG
+npx expo install react-native-svg
+
 #Others commands
 npx expo customize metro.config.js
 
